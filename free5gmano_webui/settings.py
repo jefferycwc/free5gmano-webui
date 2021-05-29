ac9429f913f43ece6b92d13a2b1a531b7c7a9012
jefferycwc <jefferynthu@gmail.com>
2021-05-29 10:49:33 +0800

    modify NM_URL

--- free5gmano_webui/settings.py
+++ free5gmano_webui/settings.py
@@ -14,8 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-NM_URL = os.getenv('NM_URL', 'http://10.20.1.111:8000/')
-
+#NM_URL = os.getenv('NM_URL', 'http://10.20.1.111:8000/')
+NM_URL = 'http://127.0.0.1:30088/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
